47f02dbf376e0057446ad17b9d61adfbc352540c
“VELLEVET” <nikgol319@yandex.ru>
2018-05-18 02:22:10 +0300

    First Info data #2

--- App.js
+++ App.js
@@ -4,6 +4,85 @@ import React, { Component } from "react";
 import { View, StyleSheet, Text, Button, ToolbarAndroid } from "react-native";
 import { TabViewAnimated, TabBar } from "react-native-tab-view";
 
+// ------------------------------------
+var msg;
+var data;
+var e = [];
+
+const login_query = {
+    id: -1,
+    method: "call",
+    params: [1,"login",["",""]]    
+}
+
+const db_connect_query = {
+    id: 0,
+    method: "call",
+    params: [1,"database",[]]    
+}
+
+const dynamic_global_props_query = {
+    id: 10,
+    method: "call",
+    params: [2,"get_dynamic_global_properties",[]]    
+}
+
+const bts_object_query = {
+    id: 11,
+    method: "call",
+    params: [2,"get_objects",[["2.3.0"]]]    
+}
+
+const global_props_query = {
+    id: 12,
+    method: "call",
+    params: [2,"get_global_properties",[]]    
+}
+
+var ws = new WebSocket('wss://bitshares.openledger.info/ws');
+
+ws.onopen = () => {
+    // connection opened
+    ws.send(JSON.stringify(login_query));           // отправляем запрос на подключение к БД
+};
+
+ws.onmessage = (e) => {
+    // a message was received
+    msg = JSON.parse(e.data);
+    //console.log("e.data = " + e.data);
+    //console.log("msg = " + msg.id);
+    
+    if (msg.id == -1) {
+        // Когда приходит ответ о подключении к БД, начинаем отсылать запросы
+        ws.send(JSON.stringify(db_connect_query)); 
+        ws.send(JSON.stringify(dynamic_global_props_query)); 
+        ws.send(JSON.stringify(bts_object_query)); 
+        ws.send(JSON.stringify(global_props_query)); 
+    }
+    
+    if (msg.id == 10) {
+        console.log("Последний блок: " + msg.result.head_block_number + " (" + msg.result.time + ", надо дату немного скорректировать)");
+    }
+    if (msg.id == 11) {
+        console.log("Текущее количество: " + msg.result[0].current_supply + " BTS (надо немного подшаманить количество)");
+        console.log("Стелс-количество: " + msg.result[0].confidential_supply + " BTS (надо немного подшаманить количество)");
+    }
+    if (msg.id == 12) {
+        console.log("Активные члены комитета: " + msg.result.active_committee_members.length);
+        console.log("Активные заверители: " + msg.result.active_witnesses.length)
+        console.log(" ");
+        console.log("Комиссии, стандартные / для пожизненной подписки (надо преобразовать все значения):");
+        console.log("Перевод: " + msg.result.parameters.current_fees.parameters[0][1].fee + " / " + msg.result.parameters.current_fees.parameters[0][1].fee * 0.2 + " BTS");
+        console.log("Цена за килобайт данных транзакции: " + msg.result.parameters.current_fees.parameters[0][1].price_per_kbyte + " / " + msg.result.parameters.current_fees.parameters[0][1].price_per_kbyte * 0.2 + " BTS");
+        console.log("Пожизненное членство: " + msg.result.parameters.current_fees.parameters[8][1].membership_lifetime_fee + " / " + " BTS");
+        
+    }
+    
+};
+
+
+// ------------------------------------
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
