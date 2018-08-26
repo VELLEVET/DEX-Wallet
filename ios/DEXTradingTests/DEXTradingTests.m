8023c0fe2736c586e63f1db345a0a85d0837a6e3
RockTea <yuravtornik@gmail.com>
2018-08-26 23:11:54 +0300

    The Great King of Commits. Again.

--- ios/DEXTradingTests/DEXTradingTests.m
+++ ios/DEXTradingTests/DEXTradingTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface dex_walletTests : XCTestCase
+@interface DEXTradingTests : XCTestCase
 
 @end
 
-@implementation dex_walletTests
+@implementation DEXTradingTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
